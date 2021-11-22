3cc0802fa78a4cb79ff628c95999e625bee40a21
chkhong <hckj1999@gmail.com>
2021-11-22 18:24:29 +0800

    added buttons

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,74 @@ export default function Home() {
   return (
     <Wrapper>
       <Map />
-      <ActionItems>Start</ActionItems>
+      <ActionItems>
+        <Header>
+          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
+          <Profile>
+            <Name>Chu Henn</Name>
+            <UserImage src="doge.jpeg" />
+          </Profile>
+        </Header>
+        <ActionButtons>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png" />
+            Ride
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.png" />
+            Wheels
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/5RjchBg/uberschedule.png" />
+            Reserve
+          </ActionButton>
+        </ActionButtons>
+        <InputButton>Where to?</InputButton>
+      </ActionItems>
     </Wrapper>
   );
 }
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300 h-screen
+  flex flex-col h-screen
 `;
 
 const ActionItems = tw.div`
-  flex-1
+  flex-1 p-4
+`;
+
+const Header = tw.div`
+  flex justify-between items-center
+`;
+
+const UberLogo = tw.img`
+  h-28
+`;
+
+const Profile = tw.div`
+  flex items-center
+`;
+
+const Name = tw.div`
+  mr-4 w-20 text-small
+`;
+
+const UserImage = tw.img`
+  h-12 w-12 rounded-full border-gray-200 p-px
+`;
+
+const ActionButtons = tw.div`
+  flex
+`;
+
+const ActionButton = tw.div`
+  flex flex-col bg-gray-200 flex-1 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl
+`;
+
+const ActionButtonImage = tw.img`
+  h-3/5
+`;
+
+const InputButton = tw.div`
+h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8 mx-1 rounded-lg
 `;
